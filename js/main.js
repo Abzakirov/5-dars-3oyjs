7217a26f5d3fec9d0150abf056b7f56deebbd244
AbdullohZokirov <abdullazokirov808@gmail.com>
2024-12-04 21:31:03 +0500

    5-darsjs

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// let str = "JavaScriptIsAmazing";
+// let boshi = 4;
+// let oxiri = 14;
+
+
+
+// let bolak = str.slice(boshi, oxiri);
+// let kichik = bolak.toLowerCase();
+// console.log(kichik);
+
+// let str = "apple orange apple banana orange";
+//  let sozlar =str.split(' ')
+//  let noyobsozlar = []
+
+//  for(let i = 0; i<sozlar.length-1;i++){
+//     if(!noyobsozlar.includes(sozlar[i])){
+//         noyobsozlar.push(sozlar[i])
+//     } else{
+//         console.log('Notogri')
+//     }
+
+// }
+
+// console.log(noyobsozlar)
+
+// let str = "  Find spaces here";
+// let trims = str.trim();
+// let strIndex = str.indexOf(trims);
+// let EndstrIndex = str.lastIndexOf(trims) + trims.length - 1;
+
+// console.log(trims,strIndex,EndstrIndex)
+// const str = "apple123orange456banana789";
+// const allowed = "abcdefghijklmnopqrstuvwxyz";
+
+// const result = str.replace(/[^a-z]/g, '');
+
+// console.log(result); 
